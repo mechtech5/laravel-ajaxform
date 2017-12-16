b5fd88631d8c46b83b08dfcd711ae33aed0b2e66
Ayush Likhar <neontechhead@gmail.com>
2017-12-16 10:03:08 +0530

    initial commit

---
+++ database/migrations/2017_12_16_041723_create_claims_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClaimsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('claims', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('barcode');
+            $table->string('your_name');
+            $table->string('item_name');
+            $table->string('date');
+            $table->string('image');
+            $table->string('req_type'); // 2 radio
+            $table->string('req_opts'); // 3 checkboxes
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('claims');
+    }
+}
